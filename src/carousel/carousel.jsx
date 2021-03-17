6ce7e8029da844d52d88de30b53b63e4791dc37a
Steph Meyering <stephane.meyering@gmail.com>
2021-03-16 20:20:43 -0700

    add 5th photo and some labels

--- src/carousel/carousel.jsx
+++ src/carousel/carousel.jsx
@@ -3,13 +3,14 @@ import img1 from "./photos/1.jpg";
 import img2 from "./photos/2.jpg";
 import img3 from "./photos/3.jpg";
 import peluche from "./photos/peluche.jpg";
+import me from "./photos/me.jpg";
 
 const MyCarousel = () => {
 
   const carouselOptions = {
-    autoFocus: true,
+    // autoFocus: true,
     autoPlay: true,
-    interval: 3000,
+    interval: 3500,
     infiniteLoop: true,
     showThumbs: false,
     showStatus: false,
@@ -19,19 +20,23 @@ const MyCarousel = () => {
 
   return (
     <Carousel {...carouselOptions}>
+      <div>
+        <img src={peluche} alt="cool cat pic" />
+        <p className="legend">This is my cat, Peluche</p>
+      </div>
+      <div>
+        <img src={me} alt="hiking in the woods" />
+        <p className="legend">This is me hiking in Muir Woods</p>
+      </div>
       <div className="img-container">
-        <img src={img1} />
-        {/* <p className="legend">Legend 1</p> */}
+        <img src={img1} alt="landscape pic from unsplash" />
+        <p className="legend">here are some landscape pics from unsplash</p>
       </div>
       <div>
-        <img src={img2} />
-        {/* <p className="legend">Legend 2</p> */}
+        <img src={img2} alt="landscape pic from unsplash" />
       </div>
       <div>
-        <img src={img3} />
-      </div>
-      <div>
-        <img src={peluche} />
+        <img src={img3} alt="landscape pic from unsplash" />
       </div>
     </Carousel>
   );
